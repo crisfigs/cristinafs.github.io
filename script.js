ed822f542b4b731e1eeeaf68d4504f0cc58a4dac
crisfigs <118774623+crisfigs@users.noreply.github.com>
2023-12-19 17:20:46 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+/* script.js */
+
+// Replace with your project links
+const project1Link = document.getElementById('project1');
+const project2Link = document.getElementById('project2');
+const project3Link = document.getElementById('project3');
+
+project1Link.addEventListener('click', () => {
+  window.open('https://project1.com');
+});
+
+project2Link.addEventListener('click', () => {
+  window.open('https://project2.com');
+});
+
+project3Link.addEventListener('click', () => {
+  window.open('https://project3.com');
+});
